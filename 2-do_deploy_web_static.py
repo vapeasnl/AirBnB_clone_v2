c77c96da695c5bfe114b4d0cf2ecc2ee355a4942
abdessamadsalim <samimalim1991@gmail.com>
2024-05-03 16:15:06 -0700

    ts upd

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
-"""
-Fabric script based on the file 1-pack_web_static.py
-"""
-
 from fabric.api import put, run, env
-from os.path import exists
+from fabric.contrib.files import exists
+import os
+
 env.hosts = ['54.162.77.178', '52.87.216.60']
 
-
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    if not os.path.exists(archive_path):
+        print("Error: Archive '{}' does not exist.".format(archive_path))
         return False
+    
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
+        file_name = os.path.basename(archive_path)
+        no_ext = os.path.splitext(file_name)[0]
+        release_path = "/data/web_static/releases/{}/".format(no_ext)
+
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('mkdir -p {}'.format(release_path))
+        run('tar -xzf /tmp/{} -C {}'.format(file_name, release_path))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {}web_static/* {}'.format(release_path, release_path))
+        run('rm -rf {}web_static'.format(release_path))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('ln -s {} /data/web_static/current'.format(release_path))
+
+        print("New version deployed!")
         return True
     except Exception as e:
-        print("Error: {}".format(str(e)))
+        print("Error deploying:", e)
         return False
